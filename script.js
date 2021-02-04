47157953a0d4ede66aa86321e7f23d7888f1c7ce
Morten Gross <mg@vuffeli.dk>
2021-02-04 22:14:43 +0100

    Game working

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 "use strict"
 
+let player;
+let computer;
+
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
     console.log("Start");
-    playersChoice();   
+
+    let rock = document.querySelector(".rock");
+    let paper = document.querySelector(".paper");
+    let scissors = document.querySelector(".scissors");
+
+    rock.dataset.id = 0;
+    paper.dataset.id = 1;
+    scissors.dataset.id = 2;
+
+    rock.addEventListener("click", playersChoice);
+    paper.addEventListener("click", playersChoice);
+    scissors.addEventListener("click", playersChoice);
+
+    // playersChoice();   
 }
 
 function playersChoice() {
     console.log("playersChoice")
+    console.log(this.dataset.id);
+
+    player = this.dataset.id;
+
     computersChoice();
 }
 
 function computersChoice() {
     console.log("computersChoice");
+
+    computer = Math.floor(Math.random() * 3);
+    console.log(computer);
+    
     showAnimation();
 }
 
@@ -24,7 +48,17 @@ function showAnimation() {
 
 function determineWinner() {
     console.log("determineWinner");
-    showWinner();
+
+    if (player == 0 && computer == 0 || player == 1 && computer == 1 || player == 2 && computer == 2) {
+        console.log("It's a draw");
+        showWinner();
+    } else if (player == 0 && computer == 2 || player == 1 && computer == 0 || player == 2 && computer == 1) {
+        console.log("Player wins");
+        showWinner();
+    } else if (player == 0 && computer == 1 || player == 1 && computer == 2 || player == 2 && computer == 0) {
+        console.log("Computer wins");
+        showWinner();
+    }
 }
 
 function showWinner() {
